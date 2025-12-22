9eb32038eb0cd59a59337d94cc2ae68d37a2885f
zuhairhk <zuhairhk@gmail.com>
2025-12-22 01:44:40 -0500

    Vercel deployment cleanup

--- backend/app/main.py
+++ backend/app/main.py
@@ -13,7 +13,7 @@ app.include_router(jobs_router)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["http://localhost:3000","https://clippyio.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
